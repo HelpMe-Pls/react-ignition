b2fca02e65a404e098fb3900b7973a6cc943022e
Random Guy <leekhoi5319990@gmail.com>
2021-02-02 11:12:48 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  let firstName = `Random `
-  let lastName = `Guy`
+  let firstName = `Random `;
+  let lastName = `Guy`;
   let fullName = firstName + lastName;
   return (
     <div className="App">
