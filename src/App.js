d24e774599f21cd918e5ab07994eda0a9ca3d017
Random Guy <leekhoi5319990@gmail.com>
2021-02-05 23:25:20 +0700

    Internal Component State

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const list = [
-    {
-      title: "React",
-      url: "https://facebook.github.io/react/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://github.com/reactjs/redux",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-  return (
-    <div className="App">
-      {list.map((item) => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      ))}
-    </div>
-  );
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  //function App() {
+  constructor(props) {
+    super(props); //sets this.props in your constructor in case you want to access them in the constructor. Otherwise, when accessing this.props in your constructor, they would be undefined.
+    this.state = {
+      list: list,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
