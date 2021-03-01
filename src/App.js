59cbc68bd958c45737428b3f6f94b484d6d45cd3
Random Guy <leekhoi5319990@gmail.com>
2021-03-01 23:15:55 +0700

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -2,35 +2,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+//set up the URL constants and default parameters to breakup the API request into chunks
+const DEFAULT_QUERY = "redux";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
 
-// The function takes the searchTerm as a param and returns another function,
-// because after all the filter function takes a function as its input.
-// The returned function has access to the item object because it is the function that is passed to the filter function.
-// In addition, the returned function will be used to filter the list based on the condition defined in the function.
-
-const isSearched = (searchTerm) => (
-  item //function isSearched(searchTerm) return function(item) return....
-) => item.title.toLowerCase().includes(searchTerm.toLowerCase());
-// match the incoming searchTerm pattern with the title property of the item from your list.
-// includes() return true when the pattern matches and the item stays in the list, the original list in the local state isn’t modified at all.
+//In JavaScript ES6, you can use template strings to concatenate your URL for the API endpoint
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
 
 class App extends Component {
   // App component uses internal state like `this.state` or `this.setState()` and life cycle methods like `constructor()` and `render()`.
@@ -42,13 +22,11 @@ class App extends Component {
     Otherwise, when accessing this.props in your constructor, they would be undefined. */
 
     this.state = {
-      list: list,
-      /* that's ES5, in ES6 it could be just (list,) ``When variable name and the state property name share the same name,
-      we can use shorthand`` */
-      searchTerm: "",
-      //  The input field should be empty in the beginning and thus the value should be an empty string
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     /* if you want to access this.state in your class method, it cannot be retrieved because this is undefined. 
@@ -57,109 +35,33 @@ class App extends Component {
 
   /* class METHODS can be autobound automatically without
   binding them explicitly by using JavaScript ES6 arrow functions. */
-  onDismiss(id) {
-    const isNotId = (item) => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList }); //store an updated list to your LOCAL state (internal component state).
+  setSearchTopStories(result) {
+    this.setState({ result }); //Once the data arrives, it changes your internal component state
   }
-  /*   If the evaluation for an item is true, the item stays in the list. Otherwise it will be filtered from the list.
-  Additionally, it is good to know that the setState function returns a new list and doesn’t mutate the old list. */
 
-  onSearchChange(event) {
-    //The event has the value of the input field in its target object
-    this.setState({ searchTerm: event.target.value }); //update the local state with the search term by using this.setState()
+  // asynchronously fetch data from the Hacker News API
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`) //It will fetch “redux” related stories, because that is the default parameter
+      .then((response) => response.json())
+      .then((result) => this.setSearchTopStories(result))
+      .catch((error) => error);
   }
 
   render() {
-    const { searchTerm, list } = this.state; // using ES6 destructuring so we can quickly access state's props
+    const { searchTerm, result } = this.state;
+    if (!result) {
+      return null;
+    }
     return (
       <div className="page">
-        <div className="interactions">
-          {/*  //## Split Component */}
-          <Search value={searchTerm} onChange={this.onSearchChange}>
-            Search:
-          </Search>
-        </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        {/* do something.... */}
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
-    );
-  }
-}
-
-// ## Split CLASS COMPONENT Definitions
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input type="text" value={value} onChange={onChange} />
-        {/* Specifies where the children should be displayed, but it has to be outside of the element.
-      After all, it is not only text that you can pass as children. You can pass an element and element trees
-      (which can be encapsulated by components again) as children. The children property makes it possible
-      to weave components into each other*/}
-      </form>
-    );
-  }
-}
-
-/* ## Refactoring class component to functional stateless component
- ## Once you would need access to its internal component state or lifecycle methods,
-## you would refactor it to an ES6 class component. */
-
-// const Search = ({ value, onChange, children }) => {
-//   //do something else..
-//   return (
-//     <form>
-//       {children} <input type="text" value={value} onChange={onChange} />
-//     </form>
-//   );
-// };
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    const largeColumn = {
-      width: "40%",
-    };
-    const midColumn = {
-      width: "30%",
-    };
-    const smallColumn = {
-      width: "10%",
-    };
-    return (
-      <div className="table">
-        {list.filter(isSearched(pattern)).map((book) => (
-          <div key={book.objectID} className="table-row">
-            <span style={largeColumn}>
-              <a href={book.url}>{book.title}</a>
-            </span>
-            <span style={midColumn}>{book.author}</span>
-            <span style={smallColumn}>{book.num_comments}</span>
-            <span style={smallColumn}>{book.points}</span>
-            <span style={smallColumn}>
-              <Button
-                onClick={() => onDismiss(book.objectID)}
-                className="button-inline"
-              >
-                Dismiss
-              </Button>
-              {/* Since you already have a button element, you can use the Button component instead. */}
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-
-class Button extends Component {
-  render() {
-    const { onClick, children, className = "" } = this.props;
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
     );
   }
 }
